1509bd36e37e0130c0e4f16a19ce9476c07bb4ad
Salamat0 <beni.eichmann@gmail.com>
2025-10-18 22:46:43 +0200

    Refactor monopoly.sh for specific emulator and link

--- monopoly.sh
+++ monopoly.sh
@@ -1,34 +1,30 @@
-#!/bin/bash
-
-function clear_app() {
-   local emulator="$1"
-    local package_name="$2"
-    echo "Clearing android data for $package_name"
-        adb -s "$emulator" shell am force-stop "$package_name"
-    adb -s "$emulator" shell pm clear "$package_name"
+local emulator="5037"
+    local package_name="Monopoly Go"
+    echo "Clearing android data for Monopoly Go"
+        adb -s "$emulator" shell am force-stop "Monopoly Go"
+    adb -s "$emulator" shell pm clear "Monopoly Go"
    
 }
 
-function open_link() {
-    local emulator="$1"
-    local link="$2"
+function open_link(mply.io/XEkWMV2t33M) {
+    local emulator="5037"
+    local link="mply.io/XEkWMV2t33M”
     echo "Opening link to $link"
      adb -s "$emulator" shell am start -a android.intent.action.VIEW -d "$link"
 }
 
-counter=0
+counter=10
 
 while [ true ]
 do
     echo "Friend Count: $counter "
 
-    clear_app "emulator-5556" "com.google.android.gms"
-    clear_app "emulator-5556" "com.scopely.monopolygo"
+    clear_app "emulator-5037" "com.google.android.gms"
+    clear_app "emulator-5037" "com.scopely.monopolygo"
 
     wait 
 
-    open_link "emulator-5556" "https://s.scope.ly/9UJObZAS6zE"
-
+    open_link "emulator-5037" "https://mply.io/XEkWMV2t33M”
     sleep 50
 
     ((counter++))
